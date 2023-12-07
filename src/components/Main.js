3c7eaea38feaa56e5c9a660f40d517d014f2f26b
kiboma <omayioben@gmail.com>
2023-12-07 17:48:26 +0300

    Fetch data from api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Main = () => {
+    const [movies, setMovies] = useState([]);
+    const movie_url="http://localhost:8000/movies/";
+
+    console.log(movies);
+
+    
+    useEffect(() =>{
+        fetch(movie_url)
+        .then(response => response.json())
+        .then(data => setMovies(data))
+
+    },[])
+
+
   return (
     <div className='main'>
+        {movies.map(movie  =>
+        <div className='card'>
+            <h1>{movie.movie_name} <span className={movie.best_rated?"best_rated":""} >{movie.best_rated?"*****":"" } </span>  </h1>
+            <p>{movie.description}</p>
+            <p>Realesed on: {movie.year_released} </p>
+        </div>
+        )}
+
       
     </div>
   )
