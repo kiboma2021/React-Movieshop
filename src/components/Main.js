87310cb4d8e7f9a84632a86841019afb4fd1004f
kiboma <omayioben@gmail.com>
2023-12-08 08:56:36 +0300

    Filter movies based on rating

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 const Main = () => {
     const [movies, setMovies] = useState([]);
-    const movie_url="http://localhost:8000/movies/";
+    const [movie_url, setMovie_ulr]=useState("http://localhost:8000/movies/");
 
     console.log(movies);
 
@@ -12,13 +12,17 @@ const Main = () => {
         .then(response => response.json())
         .then(data => setMovies(data))
 
-    },[])
+    },[movie_url])
 
 
   return (
     <div className='main'>
+        <div className='filter-btns'>
+          <button onClick={()=> setMovie_ulr("http://localhost:8000/movies/")}>All Movies</button>
+          <button onClick={()=>setMovie_ulr("http://localhost:8000/movies?best_rated=true")}>Best Rated</button>
+        </div>
         {movies.map(movie  =>
-        <div className='card'>
+        <div className='card' key={movie.id}>
             <h1>{movie.movie_name} <span className={movie.best_rated?"best_rated":""} >{movie.best_rated?"*****":"" } </span>  </h1>
             <p>{movie.description}</p>
             <p>Realesed on: {movie.year_released} </p>
